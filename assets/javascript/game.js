7f4d2573263792e5d0538a0ec775991abe6939eb
JODI BELL <jodibell@JODIs-MacBook.local>
2019-01-22 11:02:27 -0500

    finished game logic in js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,67 +3,93 @@ var wins = 0;
     losses = 0;
     userScore = 0;
 
-//We need variables to count number of times each crystal is clicked.
+//*We need variables to count number of times each crystal is clicked.
 var amethystCounter = 0;
     potassiumCounter = 0;
     sapphireCounter = 0;
     rockCounter = 0;
-
-//Need a crystalArray.
-var crystalArray = ["Amethyst", "Potassium", "Blue Sapphire", "Rock Crystal"];
+    randomAmethystValue = 0;
+    randomPotassiumValue = 0;
+    randomSapphireValue = 0;
+    randomRockValue = 0;
+    randomComputerScore = 0;
 
 //Array for computer score that starts at 19 and goes to 120.
 var computerScoreList = [];
 for (var i = 19; i<121; i++) {
     computerScoreList.push(i);
 }
-
-//Then we need to create a variable for random computer score.
-var randomComputerScore = computerScoreList [Math.floor(Math.random() * computerScoreList.length)];
-console.log(randomComputerScore);
-
 //Array for crystal values from 1-12. 
 var crystalValues = []; 
 for (var i = 1; i<13; i++) {
     crystalValues.push(i);
 }
 
-//Variables for each random crystal value.
-var randomAmethystValue = crystalValues [Math.floor(Math.random() * crystalValues.length)];
-console.log(randomAmethystValue);
+scoreReset();
 
-var randomPotassiumValue = crystalValues [Math.floor(Math.random() * crystalValues.length)];
-console.log(randomPotassiumValue);
-
-var randomSapphireValue = crystalValues [Math.floor(Math.random() * crystalValues.length)];
-console.log(randomSapphireValue);
-
-var randomRockValue = crystalValues [Math.floor(Math.random() * crystalValues.length)];
-console.log(randomRockValue);
-
-//Then, when a crystal is clicked, we need a click event that adds randomCrystalValue to the userScore.
 $("#amethyst").click(function() {
     amethystCounter++;
-    alert(amethystCounter * randomAmethystValue);
-});
+    userScore = userScore + randomAmethystValue;
+    alert("Your Score: "+ userScore);
+    scoreCheck();
+    });
 
 $("#potassium").click(function() {
     potassiumCounter++;
-    alert(potassiumCounter * randomPotassiumValue);
+    userScore = userScore + randomPotassiumValue;
+    alert("Your Score: "+ userScore);
+    scoreCheck();
 });
 
 $("#sapphire").click(function() {
     sapphireCounter++;
-    alert(sapphireCounter * randomSapphireValue);
+    userScore = userScore + randomSapphireValue;
+    alert("Your Score: "+ userScore);
+    scoreCheck();
 });
 
 $("#rock").click(function() {
     rockCounter++;
-    alert(rockCounter * randomRockValue);
+    userScore = userScore + randomRockValue;
+    alert("Your Score: "+ userScore);
+    scoreCheck();
 });
 
-//Update userScore (add all crystal totals, which means create 4 more variables, or have a long multiplication string).
+function scoreCheck() {
+    //if userScore === computerScore, then update wins++
+    if (userScore === randomComputerScore) {
+        wins++;
+        scoreReset();
+        console.log("Wins: "+ wins);
+    }
+    //else update losses++.
+    else if (userScore>randomComputerScore) {
+        losses++;
+        scoreReset();
+        console.log("Losses: "+ losses);
+    }
+}
 
-//Conditional-if userScore < computerScore, do not reset/keep adding to userScore upon click events.
-//else if userScore === computerScore, then update wins++
-//else update losses++.
+function scoreReset() {
+    amethystCounter = 0;
+    potassiumCounter = 0;
+    sapphireCounter = 0;
+    rockCounter = 0;
+    userScore = 0;
+    
+    randomAmethystValue = crystalValues [Math.floor(Math.random() * crystalValues.length)];
+    console.log(randomAmethystValue);
+
+    randomPotassiumValue = crystalValues [Math.floor(Math.random() * crystalValues.length)];
+    console.log(randomPotassiumValue);
+
+    randomSapphireValue = crystalValues [Math.floor(Math.random() * crystalValues.length)];
+    console.log(randomSapphireValue);
+
+    randomRockValue = crystalValues [Math.floor(Math.random() * crystalValues.length)];
+    console.log(randomRockValue);
+
+    randomComputerScore = computerScoreList [Math.floor(Math.random() * computerScoreList.length)];
+    console.log("Comp Score: ", randomComputerScore);
+}
+
